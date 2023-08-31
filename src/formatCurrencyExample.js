846c5c3c219d1879d98230bcf704df6707f897d7
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-08-31 05:14:28 -0600

    comentario agregado para ver si activa github actions

--- src/formatCurrencyExample.js
+++ src/formatCurrencyExample.js
@@ -1,3 +1,4 @@
+// función para poner currency de dinero a los elementos que se requieran
 export const currencyMXN = (numero) => {
     const formateador = new Intl.NumberFormat("en-US", { style: "currency", "currency": "USD" });
     const formateado = formateador.format(numero);
