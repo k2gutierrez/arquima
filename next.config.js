e17454227c025d166c0cf7c34f6227a342ca904b
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-09-01 06:46:39 -0600

    testing amplify app without image optimization

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //output: 'export',
-    //images: {
-      //  unoptimized: true,
-      //},
-      // trying to use .next/
+    images: {
+      unoptimized: true,
+    },
+    
 }
 
 module.exports = nextConfig
